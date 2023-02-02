f856bb03c5d23e6880d7224bda2df99be5bae4fb
Jessica-Mansell <jcmansell88@gmail.com>
2023-02-01 22:09:20 -0600

    Initial Commit

---
+++ L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Jessica Mansell on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
